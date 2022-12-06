9ab61d433400f3cff40287a8f3c99fe7954808e5
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-12-06 09:16:33 +0530

    d commit

---
+++ src/main/java/com/example/Libraryapp_backend/controller/libraryController.java
@@ -0,0 +1,35 @@
+package com.example.Libraryapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class libraryController {
+    @PostMapping("/")
+    public String Homepage(){
+        return "Homepage";
+    }
+
+    @PostMapping("/addbook")
+    public String addbook(){
+        return "Add book Page";
+    }
+
+    @PostMapping("/searchbook")
+    public String searchbook(){
+        return "Search book page";
+    }
+    @PostMapping("/editbook")
+    public String editbook(){
+        return "Edit book page";
+    }
+    @GetMapping("/viewallbook")
+    public String viewallbook(){
+        return "View all book page";
+    }
+    @PostMapping("/deletebook")
+    public String deletebook(){
+        return "delete book page";
+    }
+}
